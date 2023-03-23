9ddcbd5ad6b8f7d7754eb8355bb91f6d7047d557
Trevor Oler <18trevor3695@gmail.com>
2023-03-22 23:01:07 -0400

    Update Compile.py

--- Compile.py
+++ Compile.py
@@ -1,6 +1,5 @@
 import sys
 import os
 import platform
-
 os_type = sys.platform
-os.system('nuitka3 --follow-imports --output-dir=output_folder your_file_name.py')
+os.system('pyinstaller --onefile main.py')
