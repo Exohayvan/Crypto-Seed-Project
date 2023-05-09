a5510df43f7f50a2481a8bfa978d13d34ce9c741
Trevor Oler <18trevor3695@gmail.com>
2023-05-09 12:12:11 -0400

    Fixed Incorrect syntax (typo)

--- main.py
+++ main.py
@@ -529,7 +529,7 @@ elif args.version == 'true':
     print(version)
 else:
     checkInternetHttplib("www.google.com", 3)
-    checkInternetHttplib("https://blockstream.info", 10)
+    checkInternetHttplib("www.blockstream.info", 10)
     intro()
     jversion, address, is_node, userID = data_json()
     print(f"User ID for address '{address}': {userID}")
