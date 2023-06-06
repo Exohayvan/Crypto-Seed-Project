07dfa15525e3c4898a36bb2d5ba94a729d6c7dc8
Trevor Oler <18trevor3695@gmail.com>
2023-06-06 19:04:10 -0400

    Added packages.proxy (replaced def random_proxy)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from bitcoinaddress import Wallet #needed for privatekeys
 import hashlib, base64, ssl
 import math
 from collections import deque
+from packages.proxy import proxy as random_proxy
 
 msg = "Project for brute forcing private keys, using parallel processing across a network of everyone running the program! For educational use only."
 parser = argparse.ArgumentParser(description = msg)
@@ -19,7 +20,6 @@ parser.add_argument("-t", "--test", help = "Test script. Values: 'true'")
 parser.add_argument("-c", "--compile", help = "Compile script for system. Values: 'onefile' 'ipa'")
 parser.add_argument("-v", "--version", help = "Check Versions. Values: 'true'")
 args = parser.parse_args()
-
 
 #setting const
 version = 'v0.0.8-alpha'
@@ -506,27 +506,6 @@ def time_print(*args, **kwargs):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     timestamp = f"{Fore.BLUE}{Style.DIM}[{timestamp}]{Style.RESET_ALL}"
     print(timestamp, *args, **kwargs)
-def random_proxy():
-    import requests
-    import random
-    import re
-    url = 'https://free-proxy-list.net/'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        pattern = re.compile(r'<tr><td>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td><td>(\d+)</td>')
-        matches = pattern.findall(response.text)
-        ip_address, port = random.choice(matches)
-        time_print('Connecting to new proxy...')
-        proxy = f"{ip_address}:{port}"
-        time_print(Fore.BLUE + 'IP has changed to: ' + Fore.LIGHTYELLOW_EX + proxy + Fore.WHITE)
-        return proxy
-    else:
-        print(f"Error: {response.status_code}")
-
-
 
 #runtime order
 
