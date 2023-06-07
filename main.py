d6ddda091b6514f9d3aa0a8a8fa038e4e893e3a0
Trevor Oler <18trevor3695@gmail.com>
2023-06-07 02:42:07 -0400

    Added packages.coins.btc

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ import zipfile, shutil #needed for extracting zip files
 import argparse #needed for passing arguments
 import json
 import random
-from bitcoinaddress import Wallet #needed for privatekeys
 import hashlib, base64, ssl
-import math
-from collections import deque
 from packages.proxy import random_proxy
-from packages.update import w_update, r_update, cleanup, update
+from packages.update import update
+from packages.coins.btc import mining as btc_mining
 
 msg = "Project for brute forcing private keys, using parallel processing across a network of everyone running the program! For educational use only."
 parser = argparse.ArgumentParser(description = msg)
@@ -53,30 +51,6 @@ def intro():
         time_print(Fore.GREEN + f'Valid system type. {os_type.capitalize()} detected.' + Fore.WHITE)
     else:
         time_print(Fore.LIGHTRED_EX + 'Unknown system type. Please reach out to support, even if program seems to work. We want to try and add support for all systems we can. Please send us this info ' + Fore.YELLOW +'"platform: ' + os_type + '" ' + '"machine: ' + platform.machine() + '"'+ Fore.WHITE)
-
-class AverageCounter:
-    def __init__(self, intervals):
-        self.intervals = intervals
-        self.cycle_timestamps = deque()
-
-    def add_cycle(self):
-        current_time = time.time()
-        self.cycle_timestamps.append(current_time)
-        self._remove_old_cycles()
-
-    def _remove_old_cycles(self):
-        current_time = time.time()
-        while self.cycle_timestamps and (current_time - self.cycle_timestamps[0] > max(self.intervals)):
-            self.cycle_timestamps.popleft()
-
-    def get_averages(self):
-        current_time = time.time()
-        averages = {}
-        for interval in self.intervals:
-            count = sum(1 for ts in self.cycle_timestamps if current_time - ts <= interval)
-            average = count / (interval / 60)
-            averages[interval] = math.floor(average)
-        return averages
 
 def ask_run():
     choices = {
@@ -287,78 +261,18 @@ def checkInternetHttplib(url, timeout):
     except Exception as exep:
         time_print(exep)
         sys.exit(Fore.LIGHTRED_EX + 'Error, no internet connection')
-def mining(proxy):
-    counter = AverageCounter([1 * 60, 15 * 60, 30 * 60, 60 * 60])
-    while True:
-        if proxy == 0:
-            time_print('Starting Address mining...')
-            proxy = random_proxy()
-            time.sleep(5)
-            tbal = 0
-            calls = 0
-            count = 0
-            index = 0
-        else:
-            wallet = Wallet()
-            walletout = str(wallet)
-            a, a, a, PrivateHex, a, a, a, PrivateWIF, a, a, a, a, PrivateWIFc, a, a, PublicKey, a, a, a, PublicKeyc, a, a, a, PublicAddress1, a, a, a, a, PublicAddress1c, a, a, a, PublicAddress3, a, a, a, a, PublicAddressbc1PKH, a, a, a, a, PublicAddressbc1WSH = walletout.split()
-            addresses = [ PublicAddress1, PublicAddress1c, PublicAddress3, PublicAddressbc1PKH, PublicAddressbc1WSH]
-            time_print(Fore.RED + 'Checking Private Key:' + Fore.WHITE + ' | ' + Style.DIM + PrivateHex)
-            counter.add_cycle()
-            averages = counter.get_averages()
-            time_print(f"1-min average: {averages[1 * 60]}, 15-min average: {averages[15 * 60]}, 30-min average: {averages[30 * 60]}, 60-min average: {averages[60 * 60]}")
-            with ThreadPoolExecutor(max_workers=10) as executor:
-                results = []
-                for address in addresses:
-                    results.append(executor.submit(check_balance, address, proxy, count))
-            for index, future in enumerate(results):
-                address = addresses[index]
-                result = future.result()
-                if result is not None:
-                    if isinstance(result[1], float):
-                        count += 1
-                        calls += 1
-                        time_print(f"#{count}: Address {result[0]} | {result[1]:.8f} BTC")
-
-                    else:
-                        print(f"#{index+1}: Address {result[0]} - {result[1]}")
-            if tbal < 0:
-                time_print(Fore.GREEN + 'BTC Found!')
-            if calls > 249:
-                proxy = random_proxy()
-                calls = 0
-        if args.test == 'true':
-            print('Test Completed! Exiting in 5 seconds...')
-            time.sleep(5)
-            sys.exit()
-def check_balance(address, proxy, count):
-    url = f"https://blockstream.info/api/address/{address}"
-    try:
-        response = requests.get(url, proxies={'http' : proxy})
-        response.raise_for_status()
-        data = response.json()
-        balance = (data["chain_stats"]["funded_txo_sum"] - data["chain_stats"]["spent_txo_sum"]) / 100000000.0
-        count += 0
-        return (address, balance, count)
-    except requests.exceptions.HTTPError as err:
-        return (address, f"HTTP error: {err}")
-    except requests.exceptions.RequestException as err:
-        return (address, f"Request error: {err}")
-    except KeyError as err:
-        return (address, f"Unexpected response format: {err}")
 
 def time_print(*args, **kwargs):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     timestamp = f"{Fore.BLUE}{Style.DIM}[{timestamp}]{Style.RESET_ALL}"
     print(timestamp, *args, **kwargs)
-
 #runtime order
 
 if args.test == 'true':
     checkInternetHttplib("www.google.com", 3)
     print('Testing Mode...')
     time_print(Fore.LIGHTRED_EX + Fore.YELLOW +'"platform: ' + os_type + '" ' + '"machine: ' + platform.machine() + '"'+ Fore.WHITE)
-    mining(0)
+    mining(args.test)
 elif args.compile == 'onefile':
     print('Compiling File')
     compile()
@@ -374,4 +288,4 @@ else:
     jversion, address, is_node, userID = data_json()
     print(f"User ID for address '{address}': {userID}")
     update(version, os_type)
-    mining(0)
+    btc_mining(0)
