1cca4f2e53e30fa08421ab3387efcb66051a8dbd
Trevor Oler <18trevor3695@gmail.com>
2023-06-07 02:42:32 -0400

    Update btc.py

--- packages/coins/btc.py
+++ packages/coins/btc.py
@@ -3,9 +3,62 @@ import random
 from concurrent.futures import ThreadPoolExecutor
 import requests
 from colorama import Fore, Style
-import sys
+import re
+from collections import deque
+import os, sys, time, platform, datetime
+from bitcoinaddress import Wallet
+import math
+
+class AverageCounter:
+    def __init__(self, intervals):
+        self.intervals = intervals
+        self.cycle_timestamps = deque()
+
+    def add_cycle(self):
+        current_time = time.time()
+        self.cycle_timestamps.append(current_time)
+        self._remove_old_cycles()
+
+    def _remove_old_cycles(self):
+        current_time = time.time()
+        while self.cycle_timestamps and (current_time - self.cycle_timestamps[0] > max(self.intervals)):
+            self.cycle_timestamps.popleft()
+
+    def get_averages(self):
+        current_time = time.time()
+        averages = {}
+        for interval in self.intervals:
+            count = sum(1 for ts in self.cycle_timestamps if current_time - ts <= interval)
+            average = count / (interval / 60)
+            averages[interval] = math.floor(average)
+        return averages
+
+def random_proxy():
+    url = 'https://free-proxy-list.net/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        pattern = re.compile(r'<tr><td>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td><td>(\d+)</td>')
+        matches = pattern.findall(response.text)
+        ip_address, port = random.choice(matches)
+        time_print('Connecting to new proxy...')
+        proxy = f"{ip_address}:{port}"
+        time_print(Fore.BLUE + 'IP has changed to: ' + Fore.LIGHTYELLOW_EX + proxy + Fore.WHITE)
+        return proxy
+    else:
+        print(f"Error: {response.status_code}")
+
+def time_print(*args, **kwargs):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = f"{Fore.BLUE}{Style.DIM}[{timestamp}]{Style.RESET_ALL}"
+    print(timestamp, *args, **kwargs)
 
 def mining(proxy):
+    test = 'false'
+    if proxy == 'true':
+        test = 'true'
     counter = AverageCounter([1 * 60, 15 * 60, 30 * 60, 60 * 60])
     while True:
         if proxy == 0:
@@ -45,10 +98,10 @@ def mining(proxy):
             if calls > 249:
                 proxy = random_proxy()
                 calls = 0
-        if args.test == 'true':
-            print('Test Completed! Exiting in 5 seconds...')
-            time.sleep(5)
-            sys.exit()
+            if test == 'true':
+                print('Test Completed! Exiting in 5 seconds...')
+                time.sleep(5)
+                sys.exit()
 
 def check_balance(address, proxy, count):
     url = f"https://blockstream.info/api/address/{address}"
